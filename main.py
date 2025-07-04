f1cf37f6b0dc16d2c63acdb9e4c110d1ea16de50
arbazwebvio <arbaz.ahmed@webviotechnologies.com>
2025-07-04 14:59:24 +0530

    refactor: consolidate CORS middleware configuration in FastAPI app initialization

--- main.py
+++ main.py
@@ -9,9 +9,15 @@ from routes import router
 from db.database import engine, Base
 import os
 
-# Initialize FastAPI app
 app = FastAPI()
 app.include_router(router)
+
+app.add_middleware(
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Base.metadata.drop_all(bind=engine, checkfirst=True)  
 Base.metadata.create_all(bind=engine)
@@ -25,14 +31,12 @@ app.add_middleware(SessionMiddleware, secret_key=os.getenv("SESSION_SECRET_KEY",
 from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
-    CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"], 
     allow_headers=["*"],  # Allow all headers
 )
 
-# Exception Handlers
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
     errors = []
