4e27ba1faf6686caa6f8553938e3493eb679e93c
arbazwebvio <arbaz.ahmed@webviotechnologies.com>
2025-07-04 14:40:40 +0530

    refactor: clean up comments in CORS middleware configuration

--- main.py
+++ main.py
@@ -26,9 +26,9 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allow all origins
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],  # Allow all methods
+    allow_methods=["*"], 
     allow_headers=["*"],  # Allow all headers
 )
 
