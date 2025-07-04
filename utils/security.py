8d6d39299ee4c1f5903cc52e68f91dcc30171272
arbazwebvio <arbaz.ahmed@webviotechnologies.com>
2025-07-04 18:18:59 +0530

    refactor: update login function to use username for authentication and streamline token creation

--- utils/security.py
+++ utils/security.py
@@ -4,7 +4,7 @@ from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordBearer
 from config.settings import SECRET_KEY, ALGORITHM
 from datetime import datetime, timedelta
-import os
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
@@ -17,15 +17,12 @@ def verify_password(password: str, hashed: str) -> bool:
 # def create_token(data: dict):
 #     return jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
 
-def create_token(data: dict):
-    expire = int(os.getenv("TOKEN_EXPIRE_MINUTES", 60))
-    payload = {
-        "username": data["username"],
-        "exp": expire
-    }
-    token = jwt.encode(payload, os.getenv("SECRET_KEY"), algorithm="HS256")
-    return token, expire 
-
+def create_token(data: dict, expire_minutes: int = 1440):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=expire_minutes)
+    to_encode.update({"exp": expire})
+    token = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return token
 
 
 def decode_token(token: str):
